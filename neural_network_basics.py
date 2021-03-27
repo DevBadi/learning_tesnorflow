44574f35e2b8e8013edefa2ace16d739b44cdc13
DevBadi <ayoubbouchedoub@gmail.com>
2021-03-27 13:18:18 +0100

    Create the model and train it

--- neural_network_basics.py
+++ neural_network_basics.py
@@ -7,5 +7,22 @@ from tensorflow.keras.datasets import mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 print(x_train.shape)
 print(y_train.shape)
+x_train = x_train.reshape(-1, 28 * 28).astype("float32") / 255.0
+x_test = x_test.reshape(-1, 28 * 28).astype("float32") / 255.0
+# Sequential API easy but not flexible ( One input to one Output )
 
-x_train = x_train.reshape(-1, 28*28)
+model = keras.Sequential([
+    keras.Input(shape=(28*28)),
+    layers.Dense(512, activation='relu'),
+    layers.Dense(256, activation='relu'),
+    layers.Dense(10),
+
+])
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(lr=0.001),
+    metrics=["accuracy"],
+)
+model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)
+model.evaluate(x_test, y_test)
